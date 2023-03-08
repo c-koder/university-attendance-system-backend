e4ef552d88b98eb711f8d067095801b130a97e77
c-koder <chamodnugekotuwa@gmail.com>
2023-03-08 13:58:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("rootpath")();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
+  })
+);
+
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 8080;
+
+app.listen(port, () => console.log("Server listening on port " + port));
